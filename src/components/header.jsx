9ceaba2e5daea966171b6732d2ef032e6048ffa9
Amber Kim <ambergkim@gmail.com>
2018-07-01 00:28:42 -0700

    edit title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,8 +11,8 @@ class Header extends React.Component {
         <div id="hero-block">
           <img src="/src/images/amber-kim-circle.png" alt="Amber Kim Avatar" id="avatar"/>
           <div className="cta-block">
-            <h1>Full Stack JavaScript Developer</h1>
-            <h3>Doer | Lifelong Learner</h3>
+            <h1>Software Developer</h1>
+            <h3>Background in Web Design + Marketing</h3>
             <a href="mailto:amber@amberkimdesign.com">
               <button className="cta-btn">let's connect</button>
             </a>
