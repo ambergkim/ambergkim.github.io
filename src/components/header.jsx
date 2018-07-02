9509392b5b5ef097802063d144ab25cf53de111a
Amber Kim <ambergkim@gmail.com>
2018-07-02 01:18:30 -0700

    rearrange header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,17 +7,23 @@ class Header extends React.Component {
 
   render() {
     return <header>
-        <img src="/src/images/amber-kim-logo.png" alt="Amber Kim Logo" id="logo"/>
+        <nav class="header-nav">
+          <img src="/src/images/amber-kim-logo.png" alt="Amber Kim Logo" id="logo"/>
+          <div class="nav-links">
+            <a id="about-link" href="#about-section">A</a>
+            <a href="mailto:amber@amberkimdesign.com">E</a>
+            <a href="https://www.linkedin.com/in/ambergkim/">L</a>
+            <a href="https://github.com/ambergkim">G</a>
+            <a href="https://docs.google.com/document/d/18TJfPpLyXWLke-iZx0MA0V2IJn9cA8BvoQGr4VBfiu0/edit?usp=sharing">R</a>
+          </div>
+        </nav>
         <div id="hero-block">
           <img src="/src/images/amber-kim-circle.png" alt="Amber Kim Avatar" id="avatar"/>
           <div className="cta-block">
             <h1>Software Developer</h1>
             <h3>Background in Web Design + Marketing</h3>
             <a href="mailto:amber@amberkimdesign.com">
-              <button className="cta-btn">let's connect</button>
-            </a>
-            <a id="about-link" href="#about-section">
-              about me
+              <button className="cta-btn">connect</button>
             </a>
           </div>
         </div>
